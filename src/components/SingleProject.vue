9c2e2e88a07337d2433569dabfa0bdebc6400399
haikalrozhan <haikalrozhan1992@yahoo.com>
2022-04-23 01:34:03 +0800

    handle delete

--- src/components/SingleProject.vue
+++ src/components/SingleProject.vue
@@ -4,7 +4,7 @@
           <h3 @click="showDetails = !showDetails">{{project.title}}</h3>
           <div class="icons">
               <span class="material-symbols-outlined">edit</span>
-              <span class="material-symbols-outlined">delete</span>
+              <span @click="deleteProject"  class="material-symbols-outlined">delete</span>
               <span class="material-symbols-outlined">done</span>
           </div>
       </div>
@@ -19,7 +19,15 @@ export default {
 props: ['project'],
 data(){
     return{
-        showDetails : false
+        showDetails : false,
+        uri: 'http://localhost:3000/projects/' + this.project.id
+    }
+},
+methods: {
+    deleteProject(){
+        fetch(this.uri, {method: 'DELETE'})
+        .then(() => this.$emit('delete', this.project.id))
+        .catch(err => console.log(err.message))
     }
 }
 }
