bdd5a0ea6beac491e9ea36da95a0e917217baff6
haikalrozhan <haikalrozhan1992@yahoo.com>
2022-04-22 23:54:34 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  
+  <router-view/>
+</template>
+
+<style>
+#app {
+  
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  max-width: 600px;
+  margin: 0 auto;
+  color: #555;
+  
+}
+body{
+  background: #f2f2f2;
+}
+
+
+</style>
