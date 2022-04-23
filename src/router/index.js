62adc2bac6fc0e5dcf4911bbf389337bc844dc57
haikalrozhan <haikalrozhan1992@yahoo.com>
2022-04-23 22:37:32 +0800

    AddProject views

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,23 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/Home.vue";
+import AddProject from "../views/AddProject";
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: Home
+    path: "/",
+    name: "home",
+    component: Home,
   },
- 
-]
+  {
+    path: "/add",
+    name: "AddProject",
+    component: AddProject,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
