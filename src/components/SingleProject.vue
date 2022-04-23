f4e57f2490bd2c313859aae9ef5876c9f97da512
haikalrozhan <haikalrozhan1992@yahoo.com>
2022-04-23 23:21:10 +0800

    EditProject route

--- src/components/SingleProject.vue
+++ src/components/SingleProject.vue
@@ -3,7 +3,9 @@
       <div class="actions">
           <h3 @click="showDetails = !showDetails">{{project.title}}</h3>
           <div class="icons">
-              <span class="material-symbols-outlined">edit</span>
+              <router-link :to="{name: 'EditProject', params: {id: project.id}}">
+                <span class="material-symbols-outlined">edit</span>
+              </router-link>
               <span @click="deleteProject"  class="material-symbols-outlined">delete</span>
               <span @click="toggleComplete" class="material-symbols-outlined tick">done</span>
           </div>
